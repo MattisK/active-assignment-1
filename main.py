4d1b2f852d08f09ce2e643d22e0de68581d2f5ca
Ro Nanak Prasad Lacoul <nanaklacoulbus@gmail.com>
2026-02-28 17:13:44 +0100

    har lavet en analyse del til dataen der ikke er der endnu, men forhåbentligt virker det fint. Derudover har jeg ændret vores statiske analyse i overleaf, efter at have husket afhængigheder.

--- main.py
+++ main.py
@@ -488,7 +488,7 @@ if __name__ == "__main__":
     # Build configuration comments to write at the top of CSVs
     config_comments = [
         ["# Configuration"],
-        ["# N_FOLDS", N_FOLDS],
+        ["# N_FOLDS", N_FOLDS],    
         ["# EPOCHS_PER_TRIAL", EPOCHS_PER_TRIAL],
         ["# EARLY_STOPPING_PATIENCE", EARLY_STOPPING_PATIENCE],
         ["# INITIAL_POINTS", INITIAL_POINTS],
